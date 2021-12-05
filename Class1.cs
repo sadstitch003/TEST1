7c7421a92877a7d318ecd2ff15cebb4d45976024
Christian Sad <christian.kartika1@gmail.com>
2021-12-06 02:36:33 +0700

    waeaw

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST1
+{
+    internal class Class1
+    {
+        Console.Write("hai");
+    }
+}
